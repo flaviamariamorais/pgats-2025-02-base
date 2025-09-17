f35a00e68351d91e8dab79a3a1f45ab53b28d0fa
unknown <flavia.morais.mao@gmail.com>
2025-09-16 21:44:12 -0400

    alteracao no server.js

--- rest/server.js
+++ rest/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(4000, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
